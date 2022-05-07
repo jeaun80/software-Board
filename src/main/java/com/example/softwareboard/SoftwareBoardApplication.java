1b8d3cec96242d4acc851369b1678d0c63e26050
jeaun80 <kims1706@naver.com>
2022-05-07 14:11:41 +0900

    초기

---
+++ src/main/java/com/example/softwareboard/SoftwareBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.softwareboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/softwareboard/SoftwareBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softwareboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
