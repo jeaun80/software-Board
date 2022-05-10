28f3efc21d2617e07d314923c985dfd41838b249
jeaun80 <kims1706@naver.com>
2022-05-10 15:43:04 +0900

    travis test

--- src/main/java/com/example/softwareboard/controller/indexcontroller.java
+++ src/main/java/com/example/softwareboard/controller/indexcontroller.java
@@ -1,6 +1,10 @@
 package com.example.softwareboard.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +12,12 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@Api(value = "indexController")
 public class indexcontroller {
+    @ApiOperation(value = "test", notes = "테스트입니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK~!"), @ApiResponse(code = 404, message = "page not found!!!")
+    })
 
     @GetMapping("hello") public List<String> hello() { return Arrays.asList("안녕하세요", "Hello"); }
 
