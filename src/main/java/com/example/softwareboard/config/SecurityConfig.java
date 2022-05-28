6445964daa04765f7cb32adf972803f03c9875f0
jeaun80 <kims1706@naver.com>
2022-05-28 18:18:31 +0900

    .

--- src/main/java/com/example/softwareboard/config/SecurityConfig.java
+++ src/main/java/com/example/softwareboard/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers( "/auth/**", "/js/**", "/css/**", "/image/**").permitAll()
+                .antMatchers( "/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
